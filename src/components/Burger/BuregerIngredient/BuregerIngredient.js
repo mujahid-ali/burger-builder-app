cc32ba0a3004fe0f7673943c571d5512c29c5575
mujahid-ali <mujahidali0611@gmail.com>
2020-05-07 15:19:25 +0530

    Added price for burger

--- src/components/Burger/BuregerIngredient/BuregerIngredient.js
+++ src/components/Burger/BuregerIngredient/BuregerIngredient.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './BuregerIngredient.module.css'
-import BurgerBuilder from '../../../containers/BurgerBuilder/Burgeruilder';
+import BurgerBuilder from '../../../containers/BurgerBuilder/BurgerBuilder';
 
 class BurgerIngredient extends Component{
     render () {
