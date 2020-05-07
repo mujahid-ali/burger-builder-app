642490b12395e69f105146100b5f8929a3167526
mujahid-ali <mujahidali0611@gmail.com>
2020-05-07 14:46:12 +0530

    Add ingredients to burger

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { lable: 'Salad', type: 'salad'},
+    { lable: 'Bacon', type: 'bacon'},
+    { lable: 'Cheese', type: 'cheese'},
+    { lable: 'Meat', type: 'meat'},
+    
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl
+                key={ctrl.lable}
+                lable={ctrl.lable}
+                added={() =>props.ingredientAdded(ctrl.type)} />
+        ))}
+
+    </div>
+)
+
+export default buildControls;
