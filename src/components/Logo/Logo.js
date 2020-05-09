4e2dfe17e9a64a1a94d526574fc437021d2bf250
mujahid-ali <mujahidali0611@gmail.com>
2020-05-09 14:02:56 +0530

    Added: SideDrawer for mobile devices

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import brugerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.module.css';
 
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={brugerLogo} alt="MyBurger" />
     </div>
 );
