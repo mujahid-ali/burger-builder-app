530dfd46daae9ad98b0b74e3ee912c055e3b5b32
mujahid-ali <mujahidali0611@gmail.com>
2020-05-09 13:09:43 +0530

    Added Naviagtion Items

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import brugerLogo from '../../assets/images/burger-logo.png';
+import classes from './Logo.module.css';
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={brugerLogo} alt="MyBurger" />
+    </div>
+);
+
+export default logo;
